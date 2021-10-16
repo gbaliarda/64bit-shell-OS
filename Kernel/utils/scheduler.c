8838f53a929e84b4628d061ff832d6ada5b45c5f
Gonzalo Baliarda <gbaliarda@itba.edu.ar>
2021-10-16 17:55:17 -0300

    Scheduler algorithm round robin w/ priority implementation

--- Kernel/utils/scheduler.c
+++ Kernel/utils/scheduler.c
@@ -4,7 +4,8 @@
 #include "../include/lib.h"
 
 #define QUANTUM 4
-// #define MAX_CANTIDAD_PROCESOS 10
+#define MAX_CANTIDAD_PROCESOS 10
+#define NULL ((void*) 0)
 
 typedef struct pcb {
   uint32_t pid;
@@ -13,63 +14,79 @@ typedef struct pcb {
   uint64_t sp;
 } pcb;
 
-// typedef struct ListNode {
-//   pcb process;
-//   struct ListNode *next; 
-// } ListNode;
+typedef struct ListNode {
+  pcb process;
+  struct ListNode *next; 
+} ListNode;
 
 typedef struct Scheduler {
-  pcb processList[10];
-  // ListNode *start;
-  uint32_t index;
-  uint32_t currentProcess;
   uint32_t quantum;
+  ListNode *currentProcess;
+  ListNode *start;  // lista ordenada por prioridades, los mas prioritarios primero
 } Scheduler;
 
 Scheduler *scheduler;
 uint8_t firstProcess;
-uint32_t pid = 0;
+uint32_t pid;
 
 void initScheduler() {
-  scheduler = (Scheduler *) alloc(sizeof(Scheduler));
-  scheduler->index = 0;
-  scheduler->currentProcess = 0;
+  scheduler = (Scheduler *) alloc(sizeof(Scheduler) + sizeof(ListNode)*MAX_CANTIDAD_PROCESOS);
+  scheduler->currentProcess = NULL;
   scheduler->quantum = QUANTUM;
-  // scheduler->start = (ListNode *)((uint64_t)scheduler + sizeof(Scheduler));
+  scheduler->start = (ListNode *)((uint64_t)scheduler + sizeof(Scheduler));
+  scheduler->start->next = NULL;
+  pid = 0;
   firstProcess = 1;
 }
 
-static void loadProcess(uint32_t pid, uint8_t pstate, uint8_t priority, uint64_t sp) {
-  scheduler->processList[scheduler->index].pid = pid;
-  scheduler->processList[scheduler->index].pstate = pstate;
-  scheduler->processList[scheduler->index].priority = priority;
-  scheduler->processList[scheduler->index++].sp = sp;
+static ListNode *loadProcess(ListNode * node, uint32_t pid, uint8_t priority, uint64_t sp) {
+  if (node->next != NULL && node->next->process.priority > priority) {
+    node->next = loadProcess(node->next, pid, priority, sp);
+    return node;
+  }
+
+  ListNode *newNode = (ListNode *) ((uint64_t)node + sizeof(ListNode));
+
+  newNode->next = node->next;
+  node->next = newNode;
+  
+  newNode->process.pid = pid;
+  newNode->process.pstate = 1; // por defecto pstate = 1 = ready
+  newNode->process.priority = priority;
+  newNode->process.sp = sp;
+  return newNode;
 }
 
 void createProcess(uint64_t ip, uint32_t size, uint8_t priority) {
   uint64_t process = (uint64_t) alloc(size);
 	uint64_t sp = initProcess(process + size, ip);
-  loadProcess(pid++, 1, priority, sp);
+  scheduler->start = loadProcess(scheduler->start, pid++, priority, sp);
 }
 
 uint64_t switchProcess(uint64_t sp) {
   // Para el primer proceso no actualizamos el sp, usamos el hardcodeado
   if (firstProcess) {
     firstProcess = 0;
-    return scheduler->processList[scheduler->currentProcess].sp;
+    scheduler->currentProcess = scheduler->start;
+    return scheduler->start->process.sp;
   }
   if(scheduler->quantum > 0) {
     scheduler->quantum--;
     return 0;
   }
   scheduler->quantum = QUANTUM;
+
   // guardar el sp del proceso actual en su PCB
-  scheduler->processList[scheduler->currentProcess].sp = sp;
+  scheduler->currentProcess->process.sp = sp;
+  
+  ListNode * nodeIter = scheduler->start;
+  while(nodeIter != NULL && nodeIter->process.pstate != 1)
+    nodeIter = nodeIter->next;
 
-  // ROUND ROBIN CON PRIORIDADES
+  // deberia haber un dummy para que siempre haya un proceso ready, por lo que esto no podria pasar
+  if(nodeIter == NULL)
+    return 0;
 
-  // A -> B -> C -> D ;
-
-  // retornar el sp del proximo proceso a ejecutarse
-  return scheduler->processList[scheduler->currentProcess].sp;
+  scheduler->currentProcess = nodeIter;
+  return nodeIter->process.sp;
 }
